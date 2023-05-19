ce0373ceb46eefaa472bc32057a3faa6139d084c
Ian Scott Martin <ianscottmartin@gmail.com>
2023-05-18 19:49:48 -0600

    finit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -16,13 +17,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+{/* write an <About> component here */ }
+function About() {
+  return (
+    <div id="about">
+      <h2>About
+      </h2>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
